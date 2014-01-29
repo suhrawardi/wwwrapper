32fc9b545bb94a250d60e8f113d0dfcb61d487d6
jarra <suhrawardi@gmail.com>
2014-01-29 16:24:05 +0100

    Minor refactoring.

--- lib/wwwrapper/cli.ex
+++ lib/wwwrapper/cli.ex
@@ -1,29 +1,27 @@
 defmodule Wwwrapper.CLI do
 
   def run(argv) do
-    argv
-      |> parse_args
-      |> process
+    argv |> parse_args |> process
   end
 
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches:  [ help: :boolean],
-                                     aliases:   [ h:    :help])
+    parse = OptionParser.parse(argv, switches: [help: :boolean],
+                                     aliases:  [h:    :help])
     case parse do
-      {[help: true], _, _}           -> :help
-      {_, ["download", url], _}      -> {:download, url}
-      _ -> :help
+      {[help: true], _, _} -> :help
+      {_, [url], _}        -> url
+      _                    -> :help
     end
   end
 
   def process(:help) do
     IO.puts """
-      usage: crawler download <url>
+      usage: crawler <url>
     """
     System.halt(0)
   end
 
-  def process({:download, url}) do
+  def process(url) do
     Wwweaver.Process.start(url, &Wwwrapper.Downloader.save(&1, &2))
   end
 end
